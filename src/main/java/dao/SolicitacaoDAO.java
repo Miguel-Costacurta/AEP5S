c056b4c5038c5fb0aa06741f0d757ef3f6797490
Miguel-Costacurta <miguelfcostacurta@gmail.com>
2026-04-09 21:49:00 -0300

    Adicionando historico as solicitacoes
    # Conflicts:
    #	data/centraldb.mv.db
    #	src/main/java/Main.java

--- src/main/java/dao/SolicitacaoDAO.java
+++ src/main/java/dao/SolicitacaoDAO.java
@@ -120,6 +120,32 @@ public class SolicitacaoDAO {
         return null;
     }
 
+    public Solicitacao buscarPorId(int id) throws SQLException {
+        String sql = "SELECT * FROM solicitacoes WHERE id = ?";
+
+        try (Connection conn = DataBaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    Solicitacao s = new Solicitacao();
+                    s.setSolicitacaoId(rs.getInt("id"));
+                    s.setSolicitacaoProtocolo(rs.getString("protocolo"));
+                    s.setTipoSolicitacao(enums.TipoSolicitacao.valueOf(rs.getString("tipo")));
+                    s.setStatusSolicitacao(StatusSolicitacao.valueOf(rs.getString("status")));
+                    s.setPrioridade(enums.Prioridade.valueOf(rs.getString("prioridade")));
+                    s.setDescricao(rs.getString("descricao"));
+                    s.setUsuarioId(rs.getInt("usuario_id"));
+                    return s;
+                }
+            }
+        }
+
+        return null;
+    }
+
     public void atualizarStatus(int id, StatusSolicitacao novoStatus) throws SQLException {
         String sql = "UPDATE solicitacoes SET status = ? WHERE id = ?";
 
